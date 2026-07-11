4cb3a9e14f72f9f8ff6cb9c1722d004162067b9f
Vedant Raut <rautvedant563@gmail.com>
2026-07-11 10:11:00 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+We are hiring a Python Backend Developer with experience in:
+
+- Flask or Django
+- REST API Development
+- PostgreSQL or MySQL
+- Docker (optional)
+- AWS or Cloud Deployment (preferred
